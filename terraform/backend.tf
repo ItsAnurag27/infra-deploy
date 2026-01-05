3df62ef89ebc6828d99381956c992ffa3ab37384
itsAnurag27 <anuragguptap0@gmail.com>
2026-01-05 11:05:16 +0530

    running for standup

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket  = "terraform-state-anuraggupta"
     key     = "cn-web/terraform.tfstate"
     region  = "us-east-1"
-    use_lockfile = true
     # dynamodb_table = "terraform-locks"
     encrypt = true
   }
